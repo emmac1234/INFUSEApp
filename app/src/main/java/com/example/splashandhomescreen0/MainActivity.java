f24d11790fa1206aba1f4ff5d7a3fa113f5ac623
emmac1234 <136448729+emmac1234@users.noreply.github.com>
2023-06-22 09:35:36 +0100

    splashandHomescreen0.1

---
+++ app/src/main/java/com/example/splashandhomescreen0/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.splashandhomescreen0;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//        Button button1 = findViewById(R.id.button_girls);
+//        button1.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, girlsInICTMain.class);
+//            startActivity(intent);
+//        });}}
+
+//        Button button2 = findViewById(R.id.button_career);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button button3 = findViewById(R.id.button_girls);
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Button button1 = findViewById(R.id.button_tech);
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button button2 = findViewById(R.id.button_internship);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button button3 = findViewById(R.id.button_school);
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Button button3 = findViewById(R.id.button_website);
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+///
+//
