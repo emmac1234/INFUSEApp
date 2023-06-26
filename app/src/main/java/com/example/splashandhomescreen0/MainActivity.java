ad6f4c8421486e20bb9e89bbf91bb0af770a1171
naoisehourican <naoise.hourican@ericsson.com>
2023-06-26 11:05:35 +0100

    name change

--- app/src/main/java/com/example/splashandhomescreen0/MainActivity.java
+++ app/src/main/java/com/example/splashandhomescreen0/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity{
 //        button1.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                Intent intent = new Intent(MainActivity.this, TechActivity.class);
 //                startActivity(intent);
 //            }
 //        });
